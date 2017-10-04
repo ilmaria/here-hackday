84ee7453d606c35176e7b85651d53f98cccade30
Ilmari Autio <ilmari.autio@almamedia.fi>
2017-10-04 10:43:30 +0300

    add map

---
+++ category-search.js
@@ -0,0 +1,28 @@
+// Obtain an Explore object through which to submit search requests:
+var explore = new H.places.Explore(platform.getPlacesService()),
+  exploreResult,
+  error;
+
+// Define search parameters:
+var params = {
+  // Look for places matching the category "eat and drink":
+  cat: 'eat-drink',
+  // Search in the Chinatown district in San Francisco:
+  in: '37.7942,-122.4070'
+};
+
+// Run a search request with parameters, headers (empty), and callback functions:
+explore.request(params, {}, onResult, onError);
+
+// Define a callback function to handle data on success:
+function onResult(data) {
+  exploreResult = data;
+}
+
+// Define a callback function to handle errors:
+function onError(data) {
+  error = data;
+}
+
+// Run a search request with parameters, headers (empty), and callback functions:
+explore.request(params, {}, onResult, onError);
--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 var platform = new H.service.Platform({
   useCIT: true,
-  app_id: "3CbRKU6RtPhIp2t4OzQI",
-  app_code: "gfHFsKFQlPRuScYyeLMIAA"
+  app_id: '3CbRKU6RtPhIp2t4OzQI',
+  app_code: 'gfHFsKFQlPRuScYyeLMIAA'
 });
 
-// Obtain an Explore object through which to submit search requests:
-var explore = new H.places.Explore(platform.getPlacesService()),
-  exploreResult,
-  error;
+var maptypes = platform.createDefaultLayers();
 
-// Define search parameters:
-var params = {
-  // Look for places matching the category "eat and drink":
-  cat: "eat-drink",
-  // Search in the Chinatown district in San Francisco:
-  in: "37.7942,-122.4070"
-};
-
-// Run a search request with parameters, headers (empty), and callback functions:
-explore.request(params, {}, onResult, onError);
-
-// Define a callback function to handle data on success:
-function onResult(data) {
-  exploreResult = data;
-}
-
-// Define a callback function to handle errors:
-function onError(data) {
-  error = data;
-}
-
-// Run a search request with parameters, headers (empty), and callback functions:
-explore.request(params, {}, onResult, onError);
+// Instantiate (and display) a map object:
+var map = new H.Map(
+  document.getElementById('mapContainer'),
+  maptypes.normal.map,
+  {
+    zoom: 13,
+    center: { lat: 60.17, lng: 24.94 }
+  }
+);
