997905886989b0c39a9386306bb52e9c90ead84e
Kuikka Maria <maria.kuikka@almamedia.fi>
2017-10-04 10:29:30 +0300

    category search added

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 var platform = new H.service.Platform({
-  app_id: '{YOUR_APP_ID}',
-  app_code: '{YOUR_APP_CODE}'
+  useCIT: true,
+  app_id: "{YOUR_APP_ID}",
+  app_code: "{YOUR_APP_CODE}"
 });
+
+// Obtain an Explore object through which to submit search requests:
+var explore = new H.places.Explore(platform.getPlacesService()),
+  exploreResult,
+  error;
+
+// Define search parameters:
+var params = {
+  // Look for places matching the category "eat and drink":
+  cat: "eat-drink",
+  // Search in the Chinatown district in San Francisco:
+  in: "37.7942,-122.4070"
+};
+
+// Run a search request with parameters, headers (empty), and callback functions:
+explore.request(params, {}, onResult, onError);
+
+// Define a callback function to handle data on success:
+function onResult(data) {
+  exploreResult = data;
+}
+
+// Define a callback function to handle errors:
+function onError(data) {
+  error = data;
+}
+
+// Run a search request with parameters, headers (empty), and callback functions:
+explore.request(params, {}, onResult, onError);
