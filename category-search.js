75742a01ed22a1f315d5d28496e4393bc384f252
Kuikka Maria <maria.kuikka@almamedia.fi>
2017-10-04 15:30:36 +0300

    category search added

--- category-search.js
+++ category-search.js
@@ -1,70 +1,86 @@
+var currentPos = "60.1733244,24.9410248";
+var markerGroups = [];
+var explore = new H.places.Explore(platform.getPlacesService()),
+  exploreResult,
+  error;
+
+// Define a callback function to handle data on success:
+function onResult(data) {
+  addPlacesToMap(data.results);
+}
+
+// Define a callback function to handle errors:
+function onError(data) {
+  error = data;
+}
+
+// This function adds markers to the map, indicating each of
+// the located places:
+function addPlacesToMap(result) {
+  markerGroups = result.items.map(function(place) {
+    var group = new H.map.Group();
+    var marker = new H.map.Marker({
+      lat: place.position[0],
+      lng: place.position[1]
+    });
+    group.addObject(marker);
+    map.addObject(group);
+
+    timeToLocation(currentPos, place.position).then(function(data) {
+      marker.addEventListener("tap", function(e) {
+        var bubble = new H.ui.InfoBubble(
+          {
+            lat: place.position[0],
+            lng: place.position[1]
+          },
+          {
+            content:
+              place.title +
+              " - " +
+              (data.response.route[0].summary.travelTime / 60).toFixed(0) +
+              " min"
+          }
+        );
+        ui.addBubble(bubble);
+      });
+    });
+
+    return group;
+  });
+}
+
+function changeCategory() {
+  var selected = document.getElementById("menu");
+  var category = selected.options[selected.selectedIndex].value;
+
+  console.log("valitsit", category);
+  map.removeObjects(markerGroups);
+  markerGroups = [];
+  var params = {
+    cat: category,
+    at: currentPos
+  };
+  explore.request(params, {}, onResult, onError);
+}
+
+// Enable the event system on the map instance:
+var mapEvents = new H.mapevents.MapEvents(map);
+
+var behavior = new H.mapevents.Behavior(mapEvents);
+
+// Create the default UI components:
+var ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
+
 (function() {
   // Obtain an Explore object through which to submit search requests:
-  var explore = new H.places.Explore(platform.getPlacesService()),
-    exploreResult,
-    error;
 
-  // Enable the event system on the map instance:
-  var mapEvents = new H.mapevents.MapEvents(map);
-
-  var behavior = new H.mapevents.Behavior(mapEvents);
-
-  // Create the default UI components:
-  var ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
-
-  var currentPos = '60.1733244,24.9410248';
   // Define search parameters:
   var params = {
     // Look for places matching the category "eat and drink":
-    cat: 'eat-drink',
+    cat: "",
     // Search in the Chinatown district in San Francisco:
     at: currentPos
   };
-
-  // Define a callback function to handle data on success:
-  function onResult(data) {
-    addPlacesToMap(data.results);
-  }
-
-  // Define a callback function to handle errors:
-  function onError(data) {
-    error = data;
-  }
-
-  // This function adds markers to the map, indicating each of
-  // the located places:
-  function addPlacesToMap(result) {
-    result.items.map(function(place) {
-      var group = new H.map.Group();
-      var marker = new H.map.Marker({
-        lat: place.position[0],
-        lng: place.position[1]
-      });
-      group.addObject(marker);
-      map.addObject(group);
-
-      timeToLocation(currentPos, place.position).then(function(data) {
-        marker.addEventListener('tap', function(e) {
-          var bubble = new H.ui.InfoBubble(
-            {
-              lat: place.position[0],
-              lng: place.position[1]
-            },
-            {
-              content:
-                place.title +
-                ' - ' +
-                (data.response.route[0].summary.travelTime / 60).toFixed(0) +
-                ' min'
-            }
-          );
-          ui.addBubble(bubble);
-        });
-      });
-
-      return marker;
-    });
-  }
 
   // Run a search request with parameters, headers (empty), and callback functions:
   explore.request(params, {}, onResult, onError);
